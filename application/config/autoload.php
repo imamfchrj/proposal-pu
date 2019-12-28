6e828f53d3d8b99dec1e71007e29422a7f43d29c
firdasafridi <firdasafridi@ymail.com>
2019-12-28 22:00:56 +0700

    Add initial asset

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,9 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array(
+    'url', 'form','cookie','security' //, 'common', 'date', 'market', 'currency', 'common_ico'
+);
 
 /*
 | -------------------------------------------------------------------
