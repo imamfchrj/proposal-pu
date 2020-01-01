74efa2510e7d98b3227284bf9ad5f44f5539f747
firdasafridi <firdasafridi@ymail.com>
2020-01-01 21:30:45 +0700

    navbar

--- application/views/default/navbar.php
+++ application/views/default/navbar.php
@@ -2,25 +2,17 @@
     <div class="slim-navbar">
       <div class="container">
         <ul class="nav">
-          <li class="nav-item with-sub">
+          <li class="nav-item">
             <a class="nav-link" href="#">
               <i class="icon ion-ios-home-outline"></i>
               <span>Dashboard</span>
             </a>
-            <div class="sub-item">
-              <ul>
-                <li><a href="index.html">Dashboard 01</a></li>
-                <li><a href="index2.html">Dashboard 02</a></li>
-                <li><a href="index3.html">Dashboard 03</a></li>
-                <li><a href="index4.html">Dashboard 04</a></li>
-                <li><a href="index5.html">Dashboard 05</a></li>
-              </ul>
-            </div><!-- sub-item -->
           </li>
+          
           <li class="nav-item with-sub mega-dropdown">
             <a class="nav-link" href="#">
               <i class="icon ion-ios-filing-outline"></i>
-              <span>UI Elements</span>
+              <span>Pengajuan Proposal</span>
             </a>
             <div class="sub-item">
               <div class="row">
@@ -98,57 +90,10 @@
               </div><!-- row -->
             </div><!-- dropdown-menu -->
           </li>
-          <li class="nav-item with-sub">
-            <a class="nav-link" href="#">
-              <i class="icon ion-ios-book-outline"></i>
-              <span>Pages</span>
-            </a>
-            <div class="sub-item">
-              <ul>
-                <li><a href="page-profile.html">Profile Page</a></li>
-                <li><a href="page-invoice.html">Invoice</a></li>
-                <li><a href="page-contact.html">Contact Manager</a></li>
-                <li><a href="page-file-manager.html">File Manager</a></li>
-                <li><a href="page-calendar.html">Calendar</a></li>
-                <li><a href="page-timeline.html">Timeline</a></li>
-                <li class="sub-with-sub">
-                  <a href="#">Pricing</a>
-                  <ul>
-                    <li><a href="page-pricing.html">Pricing 01</a></li>
-                    <li><a href="page-pricing2.html">Pricing 02</a></li>
-                    <li><a href="page-pricing3.html">Pricing 03</a></li>
-                  </ul>
-                </li>
-                <li class="sub-with-sub">
-                  <a href="page-signin.html">Sign In</a>
-                  <ul>
-                    <li><a href="page-signin.html">Signin Simple</a></li>
-                    <li><a href="page-signin2.html">Signin Split</a></li>
-                  </ul>
-                </li>
-                <li class="sub-with-sub">
-                  <a href="page-signup.html">Sign Up</a>
-                  <ul>
-                    <li><a href="page-signup.html">Signup Simple</a></li>
-                    <li><a href="page-signup2.html">Signup Split</a></li>
-                  </ul>
-                </li>
-                <li class="sub-with-sub">
-                  <a href="#">Error Pages</a>
-                  <ul>
-                    <li><a href="page-404.html">404 Not Found</a></li>
-                    <li><a href="page-505.html">505 Forbidden</a></li>
-                    <li><a href="page-500.html">500 Internal Server</a></li>
-                    <li><a href="page-503.html">503 Service Unavailable</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div><!-- dropdown-menu -->
-          </li>
           <li class="nav-item with-sub active">
             <a class="nav-link" href="#" data-toggle="dropdown">
               <i class="icon ion-ios-gear-outline"></i>
-              <span>Forms</span>
+              <span>Approval</span>
             </a>
             <div class="sub-item">
               <ul>
@@ -163,18 +108,41 @@
               </ul>
             </div><!-- dropdown-menu -->
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="page-messages.html">
-              <i class="icon ion-ios-chatboxes-outline"></i>
-              <span>Messages</span>
-              <span class="square-8"></span>
+          <li class="nav-item with-sub">
+            <a class="nav-link" href="#" data-toggle="dropdown">
+              <i class="icon ion-ios-people"></i>
+              <span>Manajemen User</span>
             </a>
+            <div class="sub-item">
+              <ul>
+                <li class="active"><a href="form-elements.html">Form Elements</a></li>
+                <li><a href="form-layouts.html">Form Layouts</a></li>
+                <li><a href="form-validation.html">Form Validation</a></li>
+                <li><a href="form-wizards.html">Form Wizards</a></li>
+                <li><a href="form-editor.html">WYSIWYG Editor</a></li>
+                <li><a href="form-select2.html">Select2</a></li>
+                <li><a href="form-rangeslider.html">Range Slider</a></li>
+                <li><a href="form-datepicker.html">Datepicker</a></li>
+              </ul>
+            </div><!-- dropdown-menu -->
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="widgets.html">
-              <i class="icon ion-ios-analytics-outline"></i>
-              <span>Widgets</span>
+          <li class="nav-item with-sub">
+            <a class="nav-link" href="#" data-toggle="dropdown">
+              <i class="icon ion-map"></i>
+              <span>Manajemen wilayah</span>
             </a>
+            <div class="sub-item">
+              <ul>
+                <li class="active"><a href="form-elements.html">Form Elements</a></li>
+                <li><a href="form-layouts.html">Form Layouts</a></li>
+                <li><a href="form-validation.html">Form Validation</a></li>
+                <li><a href="form-wizards.html">Form Wizards</a></li>
+                <li><a href="form-editor.html">WYSIWYG Editor</a></li>
+                <li><a href="form-select2.html">Select2</a></li>
+                <li><a href="form-rangeslider.html">Range Slider</a></li>
+                <li><a href="form-datepicker.html">Datepicker</a></li>
+              </ul>
+            </div><!-- dropdown-menu -->
           </li>
         </ul>
       </div><!-- container -->
