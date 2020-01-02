a0d0c601565dbf1085861f1689ad2758ea264251
firdasafridi <firdasafridi@ymail.com>
2020-01-02 21:16:40 +0700

    change navbar menu name

--- application/views/default/navbar.php
+++ application/views/default/navbar.php
@@ -16,33 +16,13 @@
             </a>
             <div class="sub-item">
               <div class="row">
-                <div class="col-lg-5">
-                  <label class="section-label">Basic Elements</label>
+                <div class="col-lg">
+                  <label class="section-label">Air Minum</label>
                   <div class="row">
                     <div class="col">
                       <ul>
-                        <li><a href="elem-accordion.html">Accordion</a></li>
-                        <li><a href="elem-alerts.html">Alerts</a></li>
-                        <li><a href="elem-buttons.html">Buttons</a></li>
-                        <li><a href="elem-cards.html">Cards</a></li>
-                        <li><a href="elem-carousel.html">Carousel</a></li>
-                        <li><a href="elem-dropdowns.html">Dropdown</a></li>
-                        <li><a href="elem-icons.html">Icons</a></li>
-                        <li><a href="elem-images.html">Images</a></li>
-                        <li><a href="elem-lists.html">Lists</a></li>
-                      </ul>
-                    </div><!-- col -->
-                    <div class="col-lg">
-                      <ul>
-                        <li><a href="elem-modal.html">Modal</a></li>
-                        <li><a href="elem-media.html">Media Object</a></li>
-                        <li><a href="elem-navigation.html">Navigation</a></li>
-                        <li><a href="elem-pagination.html">Pagination</a></li>
-                        <li><a href="elem-tooltip.html">Tooltip</a></li>
-                        <li><a href="elem-popover.html">Popover</a></li>
-                        <li><a href="elem-progress.html">Progress</a></li>
-                        <li><a href="elem-spinner.html">Spinner</a></li>
-                        <li><a href="elem-typography.html">Typography</a></li>
+                        <li><a href="#">Evaluasi Infrastruktur Spam Durolis</a></li>
+                        <li><a href="#">Konfigurasi Parameter</a></li>
                       </ul>
                     </div><!-- col -->
                   </div><!-- row -->
@@ -50,41 +30,41 @@
                 <div class="col-lg mg-t-30 mg-lg-t-0">
                   <label class="section-label">Charts</label>
                   <ul>
-                    <li><a href="chart-morris.html">Morris Charts</a></li>
-                    <li><a href="chart-flot.html">Flot Charts</a></li>
-                    <li><a href="chart-chartjs.html">ChartJS</a></li>
-                    <li><a href="chart-echarts.html">ECharts</a></li>
-                    <li><a href="chart-chartist.html">Chartist</a></li>
-                    <li><a href="chart-rickshaw.html">Rickshaw</a></li>
-                    <li><a href="chart-sparkline.html">Sparkline</a></li>
-                    <li><a href="chart-peity.html">Peity</a></li>
+                    <li><a href="#">Morris Charts</a></li>
+                    <li><a href="#">Flot Charts</a></li>
+                    <li><a href="#">ChartJS</a></li>
+                    <li><a href="#">ECharts</a></li>
+                    <li><a href="#">Chartist</a></li>
+                    <li><a href="#">Rickshaw</a></li>
+                    <li><a href="#">Sparkline</a></li>
+                    <li><a href="#">Peity</a></li>
                   </ul>
                 </div><!-- col -->
                 <div class="col-lg mg-t-30 mg-lg-t-0">
                   <label class="section-label">Maps</label>
                   <ul>
-                    <li><a href="map-google.html">Google Maps</a></li>
-                    <li><a href="map-leaflet.html">Leaflet Maps</a></li>
-                    <li><a href="map-vector.html">Vector Maps</a></li>
+                    <li><a href="#">Google Maps</a></li>
+                    <li><a href="#">Leaflet Maps</a></li>
+                    <li><a href="#">Vector Maps</a></li>
                   </ul>
 
                   <label class="section-label mg-t-20">Tables</label>
                   <ul>
-                    <li><a href="table-basic.html">Basic Table</a></li>
-                    <li><a href="table-datatable.html">Data Table</a></li>
+                    <li><a href="#">Basic Table</a></li>
+                    <li><a href="#">Data Table</a></li>
                   </ul>
                 </div><!-- col -->
                 <div class="col-lg mg-t-30 mg-lg-t-0">
                   <label class="section-label">Helper / Utilities</label>
                   <ul>
-                    <li><a href="util-background.html">Background</a></li>
-                    <li><a href="util-border.html">Border</a></li>
-                    <li><a href="util-height.html">Height</a></li>
-                    <li><a href="util-margin.html">Margin</a></li>
-                    <li><a href="util-padding.html">Padding</a></li>
-                    <li><a href="util-position.html">Position</a></li>
-                    <li><a href="util-typography.html">Typography</a></li>
-                    <li><a href="util-width.html">Width</a></li>
+                    <li><a href="#">Background</a></li>
+                    <li><a href="#">Border</a></li>
+                    <li><a href="#">Height</a></li>
+                    <li><a href="#">Margin</a></li>
+                    <li><a href="#">Padding</a></li>
+                    <li><a href="#">Position</a></li>
+                    <li><a href="#">Typography</a></li>
+                    <li><a href="#">Width</a></li>
                   </ul>
                 </div><!-- col -->
               </div><!-- row -->
@@ -97,14 +77,14 @@
             </a>
             <div class="sub-item">
               <ul>
-                <li class="active"><a href="form-elements.html">Form Elements</a></li>
-                <li><a href="form-layouts.html">Form Layouts</a></li>
-                <li><a href="form-validation.html">Form Validation</a></li>
-                <li><a href="form-wizards.html">Form Wizards</a></li>
-                <li><a href="form-editor.html">WYSIWYG Editor</a></li>
-                <li><a href="form-select2.html">Select2</a></li>
-                <li><a href="form-rangeslider.html">Range Slider</a></li>
-                <li><a href="form-datepicker.html">Datepicker</a></li>
+                <li class="active"><a href="#">Form Elements</a></li>
+                <li><a href="#">Form Layouts</a></li>
+                <li><a href="#">Form Validation</a></li>
+                <li><a href="#">Form Wizards</a></li>
+                <li><a href="#">WYSIWYG Editor</a></li>
+                <li><a href="#">Select2</a></li>
+                <li><a href="#">Range Slider</a></li>
+                <li><a href="#">Datepicker</a></li>
               </ul>
             </div><!-- dropdown-menu -->
           </li>
@@ -115,14 +95,14 @@
             </a>
             <div class="sub-item">
               <ul>
-                <li class="active"><a href="form-elements.html">Form Elements</a></li>
-                <li><a href="form-layouts.html">Form Layouts</a></li>
-                <li><a href="form-validation.html">Form Validation</a></li>
-                <li><a href="form-wizards.html">Form Wizards</a></li>
-                <li><a href="form-editor.html">WYSIWYG Editor</a></li>
-                <li><a href="form-select2.html">Select2</a></li>
-                <li><a href="form-rangeslider.html">Range Slider</a></li>
-                <li><a href="form-datepicker.html">Datepicker</a></li>
+                <li class="active"><a href="#">Form Elements</a></li>
+                <li><a href="#">Form Layouts</a></li>
+                <li><a href="#">Form Validation</a></li>
+                <li><a href="#">Form Wizards</a></li>
+                <li><a href="#">WYSIWYG Editor</a></li>
+                <li><a href="#">Select2</a></li>
+                <li><a href="#">Range Slider</a></li>
+                <li><a href="#">Datepicker</a></li>
               </ul>
             </div><!-- dropdown-menu -->
           </li>
@@ -133,14 +113,14 @@
             </a>
             <div class="sub-item">
               <ul>
-                <li class="active"><a href="form-elements.html">Form Elements</a></li>
-                <li><a href="form-layouts.html">Form Layouts</a></li>
-                <li><a href="form-validation.html">Form Validation</a></li>
-                <li><a href="form-wizards.html">Form Wizards</a></li>
-                <li><a href="form-editor.html">WYSIWYG Editor</a></li>
-                <li><a href="form-select2.html">Select2</a></li>
-                <li><a href="form-rangeslider.html">Range Slider</a></li>
-                <li><a href="form-datepicker.html">Datepicker</a></li>
+                <li class="active"><a href="#">Form Elements</a></li>
+                <li><a href="#">Form Layouts</a></li>
+                <li><a href="#">Form Validation</a></li>
+                <li><a href="#">Form Wizards</a></li>
+                <li><a href="#">WYSIWYG Editor</a></li>
+                <li><a href="#">Select2</a></li>
+                <li><a href="#">Range Slider</a></li>
+                <li><a href="#">Datepicker</a></li>
               </ul>
             </div><!-- dropdown-menu -->
           </li>
