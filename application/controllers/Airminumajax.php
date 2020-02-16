904a07f031969ef52f2f742e072ee67df870722e
firdasafridi <firdasafridi@ymail.com>
2020-02-17 06:37:06 +0700

    rata-rata

--- application/controllers/Airminumajax.php
+++ application/controllers/Airminumajax.php
@@ -1201,7 +1201,7 @@ class Airminumajax extends All_Controller {
         $data_input["biaya_lain_lain_2_6_3"] = round_custom($sum * to_percent(10),2);
         $data_input["total_investasi"] = $sum + $data_input["biaya_lain_lain_2_6_3"];
         if($data_input["unit_produksi_1_4_2"]) {
-            $data_input["harga_rata_rata_A"] = round_custom($sum / $data_input["unit_produksi_1_4_2"],2);
+            $data_input["harga_rata_rata_A"] = round_custom($sum / 1000 / $data_input["unit_produksi_1_4_2"],2);
 
             $text="Justifikasi";
             $option=$this->danger;
@@ -1213,9 +1213,13 @@ class Airminumajax extends All_Controller {
             $data_input["verifikasi"]["harga_rata_rata_A"]["option"] = $option;
             $data_input["indikator"]["harga_rata_rata_A"]["text"] = 2700000;
             $data_input["indikator"]["harga_rata_rata_A"]["option"] = $this->default;
+            if($data_input["harga_rata_rata_A"] > 0){
+                $data_input["harga_satuan"]["harga_rata_rata_A"]["text"] = $data_input["harga_rata_rata_A"];
+                $data_input["harga_satuan"]["harga_rata_rata_A"]["option"] = $this->default;
+            }
         }
         if($data_input["pelayanan_1_2_2A"]) {
-            $data_input["harga_rata_rata_B"] = round_custom($sum / $data_input["pelayanan_1_2_2A"],2);
+            $data_input["harga_rata_rata_B"] = round_custom($sum / 1000 / $data_input["pelayanan_1_2_2A"],2);
             $text="Justifikasi";
             $option=$this->danger;
             if($data_input["harga_rata_rata_B"] <= 8360) {
@@ -1227,7 +1231,7 @@ class Airminumajax extends All_Controller {
             $data_input["indikator"]["harga_rata_rata_B"]["text"] = 8360;
             $data_input["indikator"]["harga_rata_rata_B"]["option"] = $this->default;
             if($data_input["pelayanan_1_2_2A"] > 0){
-                $data_input["harga_satuan"]["harga_rata_rata_B"]["text"] = round_custom($sum / 1000 / $data_input["pelayanan_1_2_2A"],2);
+                $data_input["harga_satuan"]["harga_rata_rata_B"]["text"] = $data_input["harga_rata_rata_B"];
                 $data_input["harga_satuan"]["harga_rata_rata_B"]["option"] = $this->default;
             }
         }
